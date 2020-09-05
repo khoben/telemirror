761d1d1efed33cd12ed3ad63d2b168eec81b4b3d
khoben <extless@gmail.com>
2020-09-05 12:47:41 +0700

    Fix os import

--- settings.py
+++ settings.py
@@ -20,7 +20,7 @@ SESSION_STRING = environ['SESSION_STRING']
 # target channel for posting
 TARGET_CHAT = environ['TARGET_CHAT']
 
-DB_URL = os.environ['DATABASE_URL']
+DB_URL = environ['DATABASE_URL']
 
 # postgres credentials
 DB_NAME = environ["DB_NAME"]
