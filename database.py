fff2f09bc6f359acfb77b8b1b6fc50ccf3a145f8
khoben <extless@gmail.com>
2020-09-15 19:47:53 +0700

    Improvement work with database
    - Connection pool
    - Context manager
    - Object Mapping 'MirrorMessage' class

--- database.py
+++ database.py
@@ -1,127 +1,120 @@
 import logging
+from contextlib import contextmanager
 
 import psycopg2
-from psycopg2 import extras
-from psycopg2.extensions import AsIs
-
-from settings import (DB_URL)
+from psycopg2 import extras, pool
+from psycopg2.extensions import AsIs, ISQLQuote, adapt
 
 logger = logging.getLogger(__name__)
 
-"""
-# Table 'binding_id'
-# id                primary key
-# original_id       original message id
-# mirror_id         mirror message id
-# original_channel  original channel id
-"""
+class MirrorMessage(object):
+    """
+    Mirror message class contains message id mapping
+    original_message_id <-> mirror_message_id
+    """
+    def __init__(self, original_id: int, mirror_id: int, original_channel: int):
+        """Ctor
 
-def create_table():
-    try:
-        connection = psycopg2.connect(DB_URL)
-    except Exception as e:
-        logger.error(e)
-        connection = None
-    if connection:
-        cursor = connection.cursor()
+        Args:
+            original_id (int): Original message ID
+            mirror_id (int): Mirror message ID
+            original_channel (int): Source channel ID
+        """
+        self.original_id = original_id
+        self.mirror_id = mirror_id
+        self.original_channel = original_channel
+
+    def __str__(self):
+        return f'{self.__class__}: {self.__dict__}'
+    
+    def __repr__(self):
+        return self.__str__()
+
+    def __conform__(self, protocol):
+        if protocol is ISQLQuote:
+            return self.getquoted()
+        return None
+
+    def getquoted(self):
+        _original_id = adapt(self.original_id).getquoted().decode('utf-8')
+        _mirror_id = adapt(self.mirror_id).getquoted().decode('utf-8')
+        _original_channel = adapt(self.original_channel).getquoted().decode('utf-8')
+        return AsIs(f'{_original_id}, {_mirror_id}, {_original_channel}')
+
+class Database:
+
+    MIN_CONN = 2
+    MAX_CONN = 10
+
+    def __init__(self, connection_string):
+        self.connection_string = connection_string
+        self.connection_pool = pool.SimpleConnectionPool(self.MIN_CONN, self.MAX_CONN, self.connection_string)
+        self.__create_table()
+
+    @contextmanager
+    def __db(self):
+        con = self.connection_pool.getconn()
+        cur = con.cursor()
         try:
-            cursor.execute(
-                """
-                CREATE TABLE IF NOT EXISTS binding_id
-                (   id serial primary key not null,
-                    original_id bigint not null,
-                    mirror_id bigint not null,
-                    original_channel bigint not null
-                )
-                """
-            )
-        except Exception as e:
-            logger.error(e, exc_info=True)
-            connection.rollback()
-        else:
-            connection.commit()
-        cursor.close()
-        connection.close()
+            yield con, cur
+        finally:
+            cur.close()
+            self.connection_pool.putconn(con)
 
-
-def insert(entity):
-    try:
-        connection = psycopg2.connect(DB_URL)
-    except Exception as e:
-        logger.error(e, exc_info=True)
-        connection = None
-    if connection:
-        cursor = connection.cursor()
-        try:
-            columns = entity.keys()
-            values = entity.values()
-
-            sql_insert = 'INSERT INTO binding_id (%s) values %s'
-
+    def __create_table(self):
+        with self.__db() as (connection, cursor):
             try:
-                cursor.execute(sql_insert, (AsIs(','.join(columns)), tuple(values)))
+                cursor.execute(
+                    """
+                    CREATE TABLE IF NOT EXISTS binding_id
+                    (   id serial primary key not null,
+                        original_id bigint not null,
+                        mirror_id bigint not null,
+                        original_channel bigint not null
+                    )
+                    """
+                )
             except Exception as e:
                 logger.error(e, exc_info=True)
                 connection.rollback()
             else:
                 connection.commit()
 
-        except Exception as e:
-            logger.error(e, exc_info=True)
+    
+    def insert(self, entity: MirrorMessage):
+        with self.__db() as (connection, cursor):
+            try:
+                cursor.execute("""
+                                INSERT INTO binding_id (original_id, mirror_id, original_channel)
+                                VALUES (%s)
+                                """, (entity,))
+            except Exception as e:
+                logger.error(e, exc_info=True)
+                connection.rollback()
+            else:
+                connection.commit()
 
-        cursor.close()
-        connection.close()
+    def find_by_original_id(self, original_id: int, original_channel: int) -> MirrorMessage:
+        """Getting MirrorMessage object
 
+        Args:
+            original_id (int): Original message ID
+            original_channel (int): Source channel ID
 
-def read():
-    rows = None
-    try:
-        connection = psycopg2.connect(DB_URL)
-    except Exception as e:
-        logger.error(e, exc_info=True)
-        connection = None
-    if connection:
-        cursor = connection.cursor()
-        try:
-            cursor.execute("""
-                            SELECT original_id, mirror_id, original_channel
-                            FROM binding_id
-                            """)
-        except Exception as e:
-            logger.error(e, exc_info=True)
-        else:
-            rows = cursor.fetchall()
-        cursor.close()
-        connection.close()
-    return rows
-
-def find_by_original_id(original_id, original_channel):
-    row = None
-    try:
-        connection = psycopg2.connect(DB_URL)
-    except Exception as e:
-        logger.error(e, exc_info=True)
-        connection = None
-
-    if connection:
-        cursor = connection.cursor(
-            cursor_factory=psycopg2.extras.RealDictCursor)
-        try:
-            cursor.execute("""
-                            SELECT original_id, mirror_id, original_channel
-                            FROM binding_id
-                            WHERE original_id = %s
-                            AND original_channel = %s
-                            """, (original_id, original_channel,))
-        except Exception as e:
-            logger.error(e, exc_info=True)
-        else:
-            row = cursor.fetchone()
-        cursor.close()
-        connection.close()
-    return row
-
-create_table()
-
-if __name__ == "__main__":
-    print(read())
+        Returns:
+            MirrorMessage
+        """
+        row = None
+        with self.__db() as (connection, cursor):
+            try:
+                cursor.execute("""
+                                SELECT original_id, mirror_id, original_channel
+                                FROM binding_id
+                                WHERE original_id = %s
+                                AND original_channel = %s
+                                """, (original_id, original_channel,))
+            except Exception as e:
+                logger.error(e, exc_info=True)
+            else:
+                row = cursor.fetchone()
+        return MirrorMessage(*row) if row else None
