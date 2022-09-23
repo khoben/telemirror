32e455341c59b5405ffc6260e52faf35afcb7690
Vladimir Baev <extless@gmail.com>
2022-09-23 18:19:18 +0700

    Use `WindowsSelectorEventLoopPolicy` if Win32 and PostgresDatabase

--- main.py
+++ main.py
@@ -5,20 +5,34 @@ from config import (API_HASH, API_ID, CHAT_MAPPING, DB_URL, DISABLE_DELETE,
 from config import REMOVE_URLS_LIST as URLS_BLACKLIST
 from config import REMOVE_URLS_WHITELIST as URLS_WHITELIST
 from config import SESSION_STRING, SOURCE_CHATS, USE_MEMORY_DB
-from telemirror.messagefilters import EmptyMessageFilter, UrlMessageFilter
+from telemirror.messagefilters import (EmptyMessageFilter, MessageFilter,
+                                       UrlMessageFilter)
 from telemirror.mirroring import MirrorTelegramClient
-from telemirror.storage import InMemoryDatabase, PostgresDatabase
+from telemirror.storage import Database, InMemoryDatabase, PostgresDatabase
 
 
-async def main():
+async def init_telemirror(logger: logging.Logger, database: Database, message_filter: MessageFilter):
+    await MirrorTelegramClient(
+        SESSION_STRING,
+        api_id=API_ID,
+        api_hash=API_HASH,
+        source_chats=SOURCE_CHATS,
+        mirror_mapping=CHAT_MAPPING,
+        database=await database.async_init(),
+        message_filter=message_filter,
+        disable_edit=DISABLE_EDIT,
+        disable_delete=DISABLE_DELETE,
+        logger=logger
+    ).run()
+
+
+def main():
+    import asyncio
+    import sys
+
     logging.basicConfig()
     logger = logging.getLogger(__name__)
     logger.setLevel(level=LOG_LEVEL)
-
-    if USE_MEMORY_DB:
-        database = await InMemoryDatabase().async_init()
-    else:
-        database = await PostgresDatabase(connection_string=DB_URL).async_init()
 
     if REMOVE_URLS:
         url_message_filter = UrlMessageFilter(
@@ -26,24 +40,16 @@ async def main():
     else:
         url_message_filter = EmptyMessageFilter()
 
-    client = MirrorTelegramClient(
-        SESSION_STRING,
-        api_id=API_ID,
-        api_hash=API_HASH,
-        source_chats=SOURCE_CHATS,
-        mirror_mapping=CHAT_MAPPING,
-        database=database,
-        message_filter=url_message_filter,
-        disable_edit=DISABLE_EDIT,
-        disable_delete=DISABLE_DELETE,
-        logger=logger
-    )
-    await client.run()
+    if USE_MEMORY_DB:
+        database = InMemoryDatabase()
+    else:
+        database = PostgresDatabase(connection_string=DB_URL)
+        if sys.platform == "win32":
+            asyncio.set_event_loop_policy(
+                asyncio.WindowsSelectorEventLoopPolicy())
+
+    asyncio.run(init_telemirror(logger, database, url_message_filter))
 
 
 if __name__ == "__main__":
-    import asyncio
-    import sys
-    if sys.platform == "win32":
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(main())
+    main()
