0282a6b01ae102b3c14f01720cb9d285a5515b8a
khoben <extless@gmail.com>
2025-05-12 07:14:22 +0700

    Add generic typing to`LRUCache`

--- telemirror/misc/lrucache.py
+++ telemirror/misc/lrucache.py
@@ -1,12 +1,20 @@
 import collections
+from typing import Generic, TypeVar
+
+K = TypeVar("K")
+V = TypeVar("V")
 
 
-class LRUCache(collections.OrderedDict):
+class LRUCache(Generic[K, V], collections.OrderedDict[K, V]):
     """
     Dict with a limited length, ejecting LRUs as needed.
+
+    Args:
+        capacity (int): Maximum number of items the cache can hold.
+        free_factor (float, optional): Fraction of items to keep when purging (default: 0.5).
     """
 
-    def __init__(self, *args, capacity, free_factor=0.5, **kwargs):
+    def __init__(self, *args, capacity: int, free_factor: float = 0.5, **kwargs):
         assert capacity > 0
         assert free_factor > 0.1 and free_factor <= 1.0
         self.capacity = capacity
@@ -14,7 +22,7 @@ class LRUCache(collections.OrderedDict):
 
         super().__init__(*args, **kwargs)
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: K, value: V):
         super().__setitem__(key, value)
         super().move_to_end(key)
 
@@ -23,7 +31,7 @@ class LRUCache(collections.OrderedDict):
                 oldkey = next(iter(self))
                 super().__delitem__(oldkey)
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: K):
         val = super().__getitem__(key)
         super().move_to_end(key)
 
